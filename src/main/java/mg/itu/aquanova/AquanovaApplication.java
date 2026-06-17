2625e66164a06b577c25d56c6602d58ac4729992
fifa <fifalianacake@gmail.com>
2026-06-17 16:20:52 +0300

    init springboot

---
+++ src/main/java/mg/itu/aquanova/AquanovaApplication.java
@@ -0,0 +1,13 @@
+package mg.itu.aquanova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquanovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquanovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/itu/aquanova/AquanovaApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.itu.aquanova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquanovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
