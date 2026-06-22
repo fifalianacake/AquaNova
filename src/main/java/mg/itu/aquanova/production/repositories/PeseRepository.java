c5a474dcd90c62d13c7c06b2f94c03bb22319a77
Armel <tommyarmel2234@gmail.com>
2026-06-22 21:35:09 +0300

    Pese_Tommy

---
+++ src/main/java/mg/itu/aquanova/production/repositories/PeseRepository.java
@@ -0,0 +1,14 @@
+package mg.itu.aquanova.production.repositories;
+
+
+import mg.itu.aquanova.production.models.Pese;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PeseRepository extends JpaRepository<Pese, Long> {
+    
+    // Récupère les pesées d'un lot triées de la plus récente à la plus ancienne
+    List<Pese> findByIdLotOrderByDatePeseeDesc(Long idLot);
+}
