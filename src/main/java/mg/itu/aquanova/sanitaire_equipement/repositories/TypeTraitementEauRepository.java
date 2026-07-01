5e883d8ff564ba5a1a959e841e5c3d3bf97dec98
andry-stack <andryrflm@gmail.com>
2026-07-01 18:09:20 +0300

    removeTraitement eau

--- src/main/java/mg/itu/aquanova/sanitaire_equipement/repositories/TypeTraitementEauRepository.java
+++
@@ -1,7 +0,0 @@
-package mg.itu.aquanova.sanitaire_equipement.repositories;
-
-import mg.itu.aquanova.sanitaire_equipement.models.TypeTraitementEau;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeTraitementEauRepository extends JpaRepository<TypeTraitementEau, Long> {
-}
