5b468e46ef9b83472de644679f831f5dde2aec13
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-07-02 06:19:09 +0300

    fix repository

--- src/main/java/mg/itu/aquanova/sanitaire_equipement/repositories/MaintenanceRepository.java
+++ src/main/java/mg/itu/aquanova/sanitaire_equipement/repositories/MaintenanceRepository.java
@@ -1,12 +1,19 @@
 package mg.itu.aquanova.sanitaire_equipement.repositories;
 
 import mg.itu.aquanova.sanitaire_equipement.models.Maintenance;
+import mg.itu.aquanova.sanitaire_equipement.models.StatutInterventionEnum;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface MaintenanceRepository extends JpaRepository<Maintenance, Long> {
     List<Maintenance> findByEquipementId(Long idEquipement);
-    List<Maintenance> findByStatutIntervention(String statut);
+    List<Maintenance> findByStatutIntervention(StatutInterventionEnum statut);
+    List<Maintenance> findByCategorieMaintenanceId(Long idCategorie);
+    List<Maintenance> findByStatutInterventionAndCategorieMaintenanceId(StatutInterventionEnum statut, Long idCategorie);
+    List<Maintenance> findByEquipementIdAndStatutInterventionIn(Long idEquipement, Collection<StatutInterventionEnum> statuts);
 }
