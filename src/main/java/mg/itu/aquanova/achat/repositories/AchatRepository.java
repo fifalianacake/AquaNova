8565e1a11729702672f59b5f442354ae840509eb
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-07-03 19:09:27 +0300

    verification categorie;

--- src/main/java/mg/itu/aquanova/achat/repositories/AchatRepository.java
+++ src/main/java/mg/itu/aquanova/achat/repositories/AchatRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import mg.itu.aquanova.achat.models.Achat;
 
 public interface AchatRepository extends JpaRepository<Achat, Long>, JpaSpecificationExecutor<Achat> {
+    boolean existsByCategorieDepenseId(Long categorieId);
 }
