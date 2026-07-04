0b807a6f7af9be8e7fb6041014fba947cb6e99ae
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-07-04 10:35:48 +0300

    add fonction findByStatut

--- src/main/java/mg/itu/aquanova/referentiel/repositories/BassinsRepository.java
+++ src/main/java/mg/itu/aquanova/referentiel/repositories/BassinsRepository.java
@@ -1,6 +1,8 @@
 package mg.itu.aquanova.referentiel.repositories;
 
 import mg.itu.aquanova.referentiel.models.Bassin;
+import mg.itu.aquanova.referentiel.models.StatutBassin;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
@@ -9,5 +11,5 @@ import java.util.Optional;
 public interface BassinsRepository extends JpaRepository<Bassin, Long> {
     // Permet de vérifier facilement si une référence existe déjà
     Optional<Bassin> findByReference(String reference);
-    Optional<Bassin> findFirstByStatutNomIgnoreCase(String nomStatut);
+    Optional<Bassin> findByStatut(StatutBassin statutBassin);
 }
