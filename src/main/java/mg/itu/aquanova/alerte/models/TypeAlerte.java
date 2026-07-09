04e0b432829b6cf630534d0785a842817b8cac24
RASOLOFOMAROMALALA Mihaja <rasolo.mihaja@yahoo.com>
2026-07-09 07:40:32 +0300

    Modèle Alerte et enums (ModuleSource, TypeAlerte, NiveauCriticite, StatutAlerte)

---
+++ src/main/java/mg/itu/aquanova/alerte/models/TypeAlerte.java
@@ -0,0 +1,15 @@
+package mg.itu.aquanova.alerte.models;
+
+/**
+ * Type fonctionnel de l'alerte.
+ */
+public enum TypeAlerte {
+    STOCK_BAS,
+    MORTALITE_ELEVEE,
+    QUALITE_EAU,
+    MAINTENANCE,
+    RECOLTE_PROCHE,
+    PEREMPTION,
+    SEUIL_DEPASSE,
+    AUTRE
+}
